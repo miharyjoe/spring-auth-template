4626c4f02fc39bf8c987ce934734a3570e6ec154
miharyjoel <miharyjoel@gmail.com>
2024-03-27 22:23:47 +0300

    feat: init template project

---
+++ src/main/java/org/mihary/springauthtemplate/SpringAuthTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.mihary.springauthtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthTemplateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAuthTemplateApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/mihary/springauthtemplate/SpringAuthTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mihary.springauthtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthTemplateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
