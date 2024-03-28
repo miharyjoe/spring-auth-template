de89426a9a16c931d4de40fb8d316b0c1a9f2f65
miharyjoel <miharyjoel@gmail.com>
2024-03-28 22:32:42 +0300

    feat: add dto

---
+++ src/main/java/org/mihary/springauthtemplate/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package org.mihary.springauthtemplate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+  private String username;
+  private String password;
+}
